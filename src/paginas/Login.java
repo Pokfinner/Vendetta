a57ba5679741af371a3da9ffbc0b6e552bfbbf09
Pokfinner <finkry@gmail.com>
2022-07-21 11:49:03 +0100

    Função de Login e Registo

--- src/paginas/Login.java
+++ src/paginas/Login.java
@@ -1,5 +1,19 @@
 package paginas;
 
 public class Login {
+	
+	// Página de Login (pode fazer redirect para Register ou para Home após login)
+	public static void Login()
+	{
+		
+	}
+	
+	// Página de Register (pode fazer redirect para Register ou para Home após login)
+	public static void Register()
+	{
+		
+		
+		
+	}
 
 }
