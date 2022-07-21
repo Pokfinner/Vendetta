b80db5011908b407aa47619e23de19e4c543cad6
Pokfinner <finkry@gmail.com>
2022-07-21 11:59:31 +0100

    Atribuições de Funções a Páginas e a Membros da Equipa

--- src/paginas/Login.java
+++ src/paginas/Login.java
@@ -2,11 +2,16 @@ package paginas;
 
 public class Login {
 	
+	// João Almeida
+	
 	// Página de Login (pode fazer redirect para Register ou para Home após login)
 	public static void Login()
 	{
 		
 	}
+	
+	
+	// Américo Sousa
 	
 	// Página de Register (pode fazer redirect para Register ou para Home após login)
 	public static void Register()
--- src/paginas/Menu.java
+++ src/paginas/Menu.java
@@ -1,5 +1,78 @@
 package paginas;
 
 public class Menu {
+	
+	// Maria João
+	
+	// Página Home (pode ir às outras páginas ou fazer Logout)
+	public static void Home()
+	{
+		
+		
+		
+	}
+	
+	
+	
+	// Jorge Batista
+	
+	// Página Home (pode ir às outras páginas ou fazer Logout)
+	public static void CarregarSaldo()
+	{
+		
+		
+		
+	}
+	
+	
+	
+	// Ricardo Dias
+	
+	// Alterar Dados do Utilizador (é necessário alterar os dados do ArrayList!)
+	public static void AlterarDadosUtilizador()
+	{
+		
+		
+		
+	}
+	
+	
+	// João Alves
+	
+	// Página Mostrar Dados de Utilizador (ir ao ArrayList)
+	public static void MostrarDadosUtilizador()
+	{
+		
+		
+		
+	}
+	
+	
+
+	// Paulo Pereira
+	
+	// Comprar (consultar dados do ArrayList e remover Saldo)
+	public static void Comprar()
+	{
+		
+		
+		
+	}
+	
+	
+	
+	// Carlos Sousa etc.
+	
+	// Comprar (consultar dados do ArrayList e remover Saldo)
+	public static void CompraEfetuada()
+	{
+		
+		
+		
+	}
+
+
+
+
 
 }
