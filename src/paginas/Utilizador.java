6c1d5d54045f1a7c005827bc695644e80d7bbe2b
Pokfinner <finkry@gmail.com>
2022-07-28 09:39:32 +0100

    Inicializar dados do Utilizador

--- src/paginas/Utilizador.java
+++ src/paginas/Utilizador.java
@@ -1,5 +1,10 @@
 package paginas;
 
 public class Utilizador {
+	
+	public static double saldo = 0;
+	public static String nome = "%NOME%";
+	public static String username = "%USERNAME%";
+	public static String password = "%PASSWORD%";
 
 }
