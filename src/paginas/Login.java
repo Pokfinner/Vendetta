6e1e5c55a5cb76e352da32842c7db7864be94c96
Pokfinner <finkry@gmail.com>
2023-02-04 08:32:16 +0000

    commit da amiga

--- src/paginas/Login.java
+++ src/paginas/Login.java
@@ -2,7 +2,7 @@ package paginas;
 
 public class Login {
 	
-	// João Teste
+	// João Teste Amiga
 	
 	// Página de Login (pode fazer redirect para Register ou para Home após login)
 	public static void Login()
